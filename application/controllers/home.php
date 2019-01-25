91757cadc8d67dbe6240f3330cb9f56a3b9f82fc
farhanhabibun <farhanhabib48@gmail.com>
2019-01-25 09:34:16 +0700

    kirim pertama

---
+++ application/controllers/home.php
@@ -0,0 +1,20 @@
+<?php 
+
+class home extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->view('login_view');
+    }
+    public function register()
+    {
+        $this->load->view('register_view');
+    }
+    public function iso()
+    {
+        $this->load->view('user_view');
+    }
+
+
+}
+?>
